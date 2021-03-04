caa680e70954c0a9e7117e4fb9abf1a58113321e
Alex <vagulisal500@gmail.com>
2021-03-05 00:18:09 +0300

    polputi

---
+++ app/js/app.js
@@ -0,0 +1,22 @@
+const swiper = new Swiper('.swiper-container', {
+    // Optional parameters
+    // direction: 'vertical',
+    loop: true,
+    // Navigation arrows
+    navigation: {
+        nextEl: '.slider-btn-right1',
+        prevEl: '.slider-btn-left1',
+    },
+
+});
+const swiper1 = new Swiper('.swiper-container', {
+    // Optional parameters
+    // direction: 'vertical',
+    loop: true,
+    // Navigation arrows
+    navigation: {
+        nextEl: '.slider-btn-right',
+        prevEl: '.slider-btn-left',
+    },
+
+});
